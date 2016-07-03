8009f1e79bc45c287496597733bb28fe66feb733
Andrei <myworld@192-168-0-101.rdsnet.ro>
2016-07-03 22:58:32 +0300

    add real data in the dummy repository

--- src/app/shared/fun-place-repo.service.ts
+++ src/app/shared/fun-place-repo.service.ts
@@ -6,10 +6,10 @@ export class FunPlaceRepoService {
 
   constructor() {}
 
- public FunPlaces = [{id:1,name:"Pizza Hot", address:"Strada strazilor 2",map:{lat:44.3, lang:33.2}, rating:2},
-    {id:2,name:"Pizza Hot A", address:"Strada strazilor 2",map:{lat:44.7, lang:33.2}, rating:1.5},
-    {id:3,name:"Pizza Hot B", address:"Strada strazilor 2",map:{lat:44.9, lang:33.2}, rating:3.3},
-    {id:4,name:"Pizza Hot C", address:"Strada strazilor 2",map:{lat:46, lang:33.2}, rating:0.9}];
+ public FunPlaces = [{id:1,name:"Pizza Random", address:"Strada Strazilor 2",map:{lat:45.6579755, lang:25.6011977}, rating:2},
+    {id:2,name:"Pizza Hot", address:"Strada Prahovei 12",map:{lat:45.657972, lang:25.6076183}, rating:1.5},
+    {id:3,name:"Pizza Hut", address:"Strada Tractorul 10",map:{lat:45.6727103, lang:25.6152275}, rating:3.3},
+    {id:4,name:"Pizza Napoli", address:"Strada Uranus 5",map:{lat:45.6417654, lang:25.627382}, rating:0.9}];
 
     getPlaceById(index:number){
       return this.FunPlaces[index];
