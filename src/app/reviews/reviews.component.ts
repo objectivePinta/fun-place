1d0fa133c2be149dba440a8db720ae5db2f626b9
objectivepinta <andrei.pinta@gmail.com>
2016-07-05 23:57:00 +0300

    css the review html

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -2,26 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import {ReviewsRepoService} from "../shared/reviews-repo.service";
 import {Review} from '../shared/review';
 import { Router, ActivatedRoute }  from '@angular/router';
+import {FunPlace} from "../shared/fun-place";
+import {StarsComponent} from "../all-fun-places/stars/stars.component";
+import {FunPlaceRepoService} from "../shared/fun-place-repo.service";
 
 @Component({
     moduleId: module.id,
     selector: 'app-reviews',
     templateUrl: 'reviews.component.html',
-    styleUrls: ['reviews.component.css']
+    styleUrls: ['reviews.component.css'],
+  directives:[StarsComponent]
 })
 export class ReviewsComponent implements OnInit {
 
-    constructor(private _reviewsRepo:ReviewsRepoService,private route: ActivatedRoute) {
+    constructor(private router:Router,private _repo:FunPlaceRepoService, private _reviewsRepo:ReviewsRepoService,private route: ActivatedRoute) {
     }
 
     results:Array<Review>;
+    funPlace:FunPlace;
     private sub:any;
-
+    id:number;
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
-            let id = +params['id']; // (+) converts string 'id' to a number
-            this.results = this._reviewsRepo.getReviewsOfPlaceWithThisId(id);
+            this.id = +params['id']; // (+) converts string 'id' to a number
+             console.log(this.id);
+            this.results = this._reviewsRepo.getReviewsOfPlaceWithThisId(this.id);
+            this.funPlace = this._repo.getPlaceById(this.id-1);
         });
     }
 
+  goBack() {
+    this.router.navigate(['/place/:'+this.id]);
+  }
+
+  goToAll()
+  {
+    this.router.navigate(['/all']);
+  }
 }
--- src/app/shared/reviews-repo.service.ts
+++ src/app/shared/reviews-repo.service.ts
@@ -4,11 +4,11 @@ import {Review} from "./review";
 @Injectable()
 export class ReviewsRepoService {
 
-    REVIEWS = [{id: 1, idOfPlace: 1, content: "It's ok", rating: 2, date: new Date('18 - 02 - 1986')},
-        {id: 2, idOfPlace: 1, content: "It's quite ok", rating: 2, date: new Date('18 - 02 - 1986')},
-        {id: 3, idOfPlace: 1, content: "It's fantastic", rating: 2, date: new Date('18 - 02 - 1986')},
-        {id: 4, idOfPlace: 1, content: "It's otherworldly", rating: 2, date: new Date('18 - 02 - 1986')},
-        {id: 5, idOfPlace: 1, content: "It's ok", rating: 2, date: new Date('18 - 02 - 1986')}]
+    REVIEWS = [{id: 1, idOfPlace: 1, content: "It's ok", rating: 1, date: new Date('1968-11-16T00:00:00')},
+        {id: 2, idOfPlace: 1, content: "It's quite ok", rating: 2, date: new Date('1968-11-16T00:00:00')},
+        {id: 3, idOfPlace: 1, content: "It's fantastic", rating: 3, date: new Date('1968-11-16T00:00:00')},
+        {id: 4, idOfPlace: 1, content: "It's otherworldly", rating: 4, date: new Date('1968-11-16T00:00:00')},
+        {id: 5, idOfPlace: 1, content: "It's ok", rating: 5, date: new Date('1968-11-16T00:00:00')}]
 
     constructor() {
     }
